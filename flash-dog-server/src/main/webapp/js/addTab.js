4dc12007f79ac6f7a16e8a629968fb80db0d38cc
Steven.Zheng <Steven.Zheng@bjb0502.sky-mobi.com>
2012-11-07 17:33:23 +0800

    增加确认删除的提示

--- flash-dog-server/src/main/webapp/js/addTab.js
+++ flash-dog-server/src/main/webapp/js/addTab.js
@@ -69,7 +69,9 @@ var $tab_title_input = $( "#tab_title"),
 		// close icon: removing the tab on click
 		// note: closable tabs gonna be an option in the future - see http://dev.jqueryui.com/ticket/3924
 		$( "#tabs span.ui-icon-close" ).live( "click", function() {
-			var index = $( "li", $tabs ).index( $( this ).parent() );
-			$tabs.tabs( "remove", index );
+			if (confirm("确认删除"))  {  
+				var index = $( "li", $tabs ).index( $( this ).parent() );
+				$tabs.tabs( "remove", index );
+			}
 		});
 }
